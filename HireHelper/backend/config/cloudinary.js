cdf858dfd54e396f78e35f1ee644e9ea2fd7f258
Madhura G R <madhuragr85@gmail.com>
2026-03-22 15:40:28 +0530

    Delete HireHelper directory

--- HireHelper/backend/config/cloudinary.js
+++
@@ -1,6 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-// Use CLOUDINARY_URL (avoids env timing issues)
-cloudinary.config(process.env.CLOUDINARY_URL);
-
-export default cloudinary;
--- HireHelper/backend/routes/otp_route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { sendOtp, resetPassword } from "../controllers/otp_controller.js";
-
-const router = express.Router();
-
-router.post("/send-otp", sendOtp);
-router.post("/reset-password", resetPassword);
-
-export default router;
